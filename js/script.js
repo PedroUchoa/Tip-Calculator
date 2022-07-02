44c7c3b24b6567e3189294bfacfdac75d7554342
Joao Pedro <joaopedrouchoacampos@gmail.com>
2022-07-02 15:56:30 -0300

    Atualizando finalização para desktop

--- js/script.js
+++ js/script.js
@@ -1,34 +1,57 @@
 const buttons = document.querySelectorAll("[atualizar]")
 const custom = document.querySelector("#input-custom")
-let value = 0
-
+const valorAmount = document.getElementById('valor-amount')
+const valorTotal = document.getElementById('valor-total')
+const inputBill = document.getElementById('bill')
+const inputPeople = document.getElementById('number-people')
+let value = ' '
 
 document.querySelector('html').addEventListener("input", calcularGorjeta)
 
+document.getElementById('button-reset').addEventListener('click', resetar)
 
-buttons.forEach(Element =>{
+buttons.forEach(Element => {
     Element.addEventListener('click', mudaStatus)
 })
 
-function mudaStatus(event){
-    buttons.forEach(Element =>{
+custom.addEventListener("input", adicionarValue)
+
+function mudaStatus(event) {
+    buttons.forEach(Element => {
         Element.classList.remove('button-active')
     })
-   if(event.target.type != 'number'){     
-       event.target.classList.add('button-active')
-       custom.value = '' 
-       value = parseInt(event.target.value)
-   }else{
-    value = 0
-   }
-   calcularGorjeta()
+    if (event.target.type != 'number') {
+        event.target.classList.add('button-active')
+        custom.value = ''
+        value = parseFloat(event.target.value)
+    }
+    calcularGorjeta()
+}
+
+function adicionarValue() {
+    value = parseFloat(custom.value)
 }
 
 
+function calcularGorjeta() {
+    let bill = parseFloat(inputBill.value)
+    let people = parseFloat(inputPeople.value)
+    let gorjeta = ((bill / 100) * value) / people
+    let valor = gorjeta + (bill / people)
+    if (!isNaN(valor) && !isNaN(gorjeta) && value != '') {
+        valorTotal.innerHTML = `R$ ${valor.toFixed(2).replace('.', ',')}`
+        valorAmount.innerHTML = `R$ ${gorjeta.toFixed(2).replace('.', ',')}`
+        console.log(value)
+    }
+}
 
-
-function calcularGorjeta(){
-    let bill = parseInt(document.getElementById('bill').value)
-    let people = parseInt(document.getElementById('number-people').value)
-    console.log(bill + people + value) 
+function resetar() {
+    inputBill.value = ""
+    inputPeople.value = ''    
+    valorTotal.innerHTML= `$0.00`
+    valorAmount.innerHTML = `$0.00`
+    buttons.forEach(Element => {
+        Element.classList.remove('button-active')
+    })
+    custom.value = ''
 }
